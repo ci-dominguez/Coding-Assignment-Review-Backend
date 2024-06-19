3c43e5099766571a792cde10eccdc7d05fd333e7
Cristian <cdom127@wgu.edu>
2024-06-18 20:17:28 -0700

    Initial

---
+++ src/main/java/com/ci_dominguez/CodeReviewBackend/CodeReviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ci_dominguez.CodeReviewBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci_dominguez/CodeReviewBackend/CodeReviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci_dominguez.CodeReviewBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
